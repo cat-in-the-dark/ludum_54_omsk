a20686351d2c3cc4ae291d22c726f01af7ccfe62
Anna Gamova <gamchenkoanna@gmail.com>
2023-10-01 12:58:52 +0600

    tetris time

--- src/cubes/falling-cubes.js
+++ src/cubes/falling-cubes.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-undef */
 AFRAME.registerComponent("falling-cubes", {
   init: () => {
+    this.existingCubes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     function getRandomColor() {
       var colors = ["#FFC65D", "#FF5733", "#C70039", "#900C3F", "#581845"];
       return colors[Math.floor(Math.random() * colors.length)];
     }
 
     function getRandomPosition() {
-      var x = Math.floor(Math.random() * 11) - 5;
+      var x = (Math.floor(Math.random() * 9) - 4) * 0.25;
       var y = 15;
       var z = -0.5;
       return { x, y, z };
@@ -15,6 +16,7 @@ AFRAME.registerComponent("falling-cubes", {
 
     setInterval(() => {
       const p = getRandomPosition();
+      const index = p.x * 4 + 4;
       var cube = document.createElement("a-entity");
       cube.setAttribute("color", getRandomColor());
       cube.setAttribute("position", p);
@@ -22,14 +24,13 @@ AFRAME.registerComponent("falling-cubes", {
         "geometry",
         "primitive: box; width: 0.25; height: 0.25; depth: 0.25"
       );
-      cube.setAttribute(
-        "animation",
-        `property: position; to: ${p.x} ${0.5} ${
-          p.z
-        }; dur: 5000; easing: linear`
-      );
       cube.setAttribute("grabable", "");
+      cube.setAttribute(
+        "cube",
+        `minY: ${0.5 + this.existingCubes[index] * 0.25}`
+      );
       document.querySelector("a-scene").appendChild(cube);
+      this.existingCubes[index]++;
     }, 2000);
   },
 });
