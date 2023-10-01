87d1bc5e610d0140d5037c12ac864769e8f2536d
Ilya Siganov <ilya.blan4@gmail.com>
2023-10-01 13:32:58 +0600

    feat: refactor spawn

--- src/cubes/falling-cubes.js
+++ src/cubes/falling-cubes.js
@@ -1,36 +1,40 @@
 /* eslint-disable no-undef */
+function getRandomColor() {
+  var colors = ["#FFC65D", "#FF5733", "#C70039", "#900C3F", "#581845"];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function getRandomPosition() {
+  var x = (Math.floor(Math.random() * 9) - 4) * 0.25;
+  var y = 15;
+  var z = -0.5;
+  return { x, y, z };
+}
+
 AFRAME.registerComponent("falling-cubes", {
-  init: () => {
+  init() {
     this.existingCubes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    function getRandomColor() {
-      var colors = ["#FFC65D", "#FF5733", "#C70039", "#900C3F", "#581845"];
-      return colors[Math.floor(Math.random() * colors.length)];
-    }
 
-    function getRandomPosition() {
-      var x = (Math.floor(Math.random() * 9) - 4) * 0.25;
-      var y = 15;
-      var z = -0.5;
-      return { x, y, z };
-    }
+    this.spawnCube = this.spawnCube.bind(this);
+    setInterval(this.spawnCube, 2000);
+  },
 
-    setInterval(() => {
-      const p = getRandomPosition();
-      const index = p.x * 4 + 4;
-      var cube = document.createElement("a-entity");
-      cube.setAttribute("color", getRandomColor());
-      cube.setAttribute("position", p);
-      cube.setAttribute(
-        "geometry",
-        "primitive: box; width: 0.25; height: 0.25; depth: 0.25"
-      );
-      cube.setAttribute("grabable", "");
-      cube.setAttribute(
-        "cube",
-        `minY: ${0.5 + this.existingCubes[index] * 0.25}`
-      );
-      document.querySelector("a-scene").appendChild(cube);
-      this.existingCubes[index]++;
-    }, 2000);
+  spawnCube() {
+    const p = getRandomPosition();
+    const index = p.x * 4 + 4;
+    var cube = document.createElement("a-entity");
+    cube.setAttribute("color", getRandomColor());
+    cube.setAttribute("position", p);
+    cube.setAttribute(
+      "geometry",
+      "primitive: box; width: 0.25; height: 0.25; depth: 0.25"
+    );
+    cube.setAttribute("grabable", "");
+    cube.setAttribute(
+      "cube",
+      `minY: ${0.5 + this.existingCubes[index] * 0.25}`
+    );
+    document.querySelector("a-scene").appendChild(cube);
+    this.existingCubes[index]++;
   },
 });
